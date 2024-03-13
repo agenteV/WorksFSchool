28ac261474f3a4a3c1f8d7d847585d20c3735800
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-13 13:01:18 -0300

    EXEMPLOS DE ARRAY

--- EX - JS/script.js
+++ EX - JS/script.js
@@ -1,3 +1,40 @@
-const nomes = ['Jolonga', 98, 'Xvideos', {}]
+const aliens = ['XRL-8', 'Bala de Canhão', 'Quatro-Braços', 'Chama']
 
-console.log(nomes)
+
+
+/*FILTRA A ARRAY
+aliens.filter(elements=>{
+    console.log(elements === 'Chama')
+})
+*/ 
+
+/*MAPEIA E MODIFICA O ARRAY
+aliens.map(elements=>{
+    console.log(`Meu alien favorito é: ${elements}`)
+})
+*/ 
+
+/* PASSAR POR TODOS
+for(let i = 0; i < aliens.length; i ++){
+    console.log(aliens[i])
+}
+
+aliens.forEach(function(meusAliens){
+    console.log(meusAliens)
+})
+*/
+
+/* TROCA DE POSIÇÃO
+nomes[1]='Vithonex'
+*/ 
+
+
+/*ADCIONA
+nomes.push('Daniel')
+*/
+
+
+/*REMOVE
+nomes.pop()
+*/
+
