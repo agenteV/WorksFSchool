d2c454ab536e9a09f6cb080f5d87b6ae37727801
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 12:51:32 -0300

    Site de Cinema - Incompleto

---
+++ REACT/CineTinho/src/componentes/Header/Header.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../Navbar/Navbar";
+
+function Header() {
+    return ( 
+    <>
+    <header>
+        <span>Olá amantes de cinema!</span>
+        <h1>Bem-vindos ao CineTinho!</h1>
+        <Navbar/>
+    </header>
+    </> 
+    
+    );
+}
+
+export default Header;
---
+++ REACT/CineTinho/src/componentes/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+function Navbar() {
+    return ( <>
+    <h1> Este aqui é a NAVBAR</h1>
+    </> );
+}
+
+export default Navbar;
