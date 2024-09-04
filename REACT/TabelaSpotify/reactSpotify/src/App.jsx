a12769a2167e29f61744f7c62a84dae4a7781236
vitinho <vitorfdiamantino@gmail.com>
2024-09-04 12:16:15 -0300

    Produção de tela

---
+++ REACT/TabelaSpotify/reactSpotify/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+ 
+
+  return (
+  <>
+  <nav className='bg-pink-700 bg-cover w-full h-28'>
+  </nav>
+
+  <div className="bg-emerald-200 bg-cover w-48 h-screen p-10">
+  </div>
+
+  <div className="bg-pink-700 bg-cover w-28 h-28  flex justify-center items-center mb-10">
+  </div>
+
+  <div className="bg-pink-700 bg-cover w-28 h-28 flex justify-center items-center mb-10 ">
+  </div>
+
+  <div className="bg-pink-700 bg-cover w-28 h-28  flex justify-center items-center "></div>
+  </>
+  )
+}
+
+export default App
